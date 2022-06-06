ac5b4af0d3298a09c6ba7ec6eb27e804f616fb7c
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-06-06 18:33:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
+song1="";
+song2="";
+function preload()
+{
+song1=loadSound("believer.mp3");
+song2=loadSound("rule_the_world.mp3");
+}
 function setup()
 {
 canvas=createCanvas(600,500);
 canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+poseNet=ml5.poseNet(video,modelloaded);
+poseNet.on('pose',gotPoses);
+}
+function modelloaded()
+{
+console.log("Model is loaded");
+}
+function gotPoses(error,results)
+{
+if(error)
+{
+console.error(error);
+}
+if(results.length>0)
+{
+console.log(results);
+}
+}
+function draw()
+{
+image(video,0,0,600,500);
+}
+function play()
+{
+song.play();
+song.setVolume(1);
+song.rate(1);
 }

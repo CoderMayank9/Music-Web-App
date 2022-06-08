f7f89909d1338253411b3fecad7d833d9ce3a06f
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-06-08 18:25:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song1="";
 song2="";
+scorerightwrist=0;
+scoreleftwrist=0;
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rigthwristy=0;
 function preload()
 {
 song1=loadSound("believer.mp3");
@@ -18,20 +24,32 @@ function modelloaded()
 {
 console.log("Model is loaded");
 }
-function gotPoses(error,results)
+function gotPoses(results)
 {
-if(error)
-{
-console.error(error);
-}
 if(results.length>0)
 {
 console.log(results);
+scorerightwrist=results[0].pose.keypoints[10].score;
+scoreleftwrist=results[0].pose.keypoints[9].score;
+leftwristx=results[0].pose.leftWrist.x;
+leftwristy=results[0].pose.leftWrist.y;
+rightwristx=results[0].pose.rightWrist.x;
+rightwristy=results[0].pose.rightWrist.y;
 }
 }
 function draw()
 {
 image(video,0,0,600,500);
+fill("#FF0000");
+stroke("#FF0000");
+if(scorerightwrist>0.2)
+{
+circle(rightwristx,rightwristy,20);
+}
+if(scoreleftwrist>0.2)
+{
+circle(leftwristx,leftwristy,20);
+}
 }
 function play()
 {
